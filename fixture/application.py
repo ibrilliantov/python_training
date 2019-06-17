b87f2dec381df1e3f34d6297ee257c00b11f917e
DenisMac <lassa1@ya.ru>
2019-06-17 23:54:31 +0300

    session helper class

--- fixture/application.py
+++ fixture/application.py
@@ -1,48 +1,21 @@
 
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.session=SessionHelper(self)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
-    def return_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
+
 
     def return_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
-
-    def create_groups(self, group):
-        wd = self.wd
-        self.open_group_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_groups_page()
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def open_group_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_groups(Group(name="gdfgsdgdfg", header="gsdsgfsgf", footer="footer"))
+    app.group.create(Group(name="gdfgsdgdfg", header="gsdsgfsgf", footer="footer"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_groups(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
