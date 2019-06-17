6d7e9501af201996597e917f69831b134a189799
DenisMac <lassa1@ya.ru>
2019-06-18 00:01:23 +0300

    contact helper class

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -12,9 +12,9 @@ def app(request):
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
     # self.add_photo(wd, upload="C//")
-    app.create_contact(Contact(middlename="Iromman", firstname="Tony", lastname="Stark", nickname="ttt", title="WOW",
-                                    company="StarkInc", address="www", home="rock", work="worldsaver", mobile="123",
-                                    fax="123", email="one@home.com", email2="two@home.com", email3="three@home.com",
-                                    homepage="ewew", address2="leningrad", phone2="456", notes="what?", aday="5",
-                                    amonth="March", ayear="1987", bday="5", bmonth="March", byear="1987",))
+    app.contact.create(Contact(middlename="Iromman", firstname="Tony", lastname="Stark", nickname="ttt", title="WOW",
+                               company="StarkInc", address="www", home="rock", work="worldsaver", mobile="123",
+                               fax="123", email="one@home.com", email2="two@home.com", email3="three@home.com",
+                               homepage="ewew", address2="leningrad", phone2="456", notes="what?", aday="5",
+                               amonth="March", ayear="1987", bday="5", bmonth="March", byear="1987", ))
     app.session.logout()
