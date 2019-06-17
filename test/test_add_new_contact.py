0979eecd26c0c6e42871461ce833ed4d472ded08
DenisMac <lassa1@ya.ru>
2019-06-17 23:43:40 +0300

    session helper class

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -10,11 +10,11 @@ def app(request):
     return fixture
     
 def test_add_new_contact(app):
-    app.login(login="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     # self.add_photo(wd, upload="C//")
     app.create_contact(Contact(middlename="Iromman", firstname="Tony", lastname="Stark", nickname="ttt", title="WOW",
                                     company="StarkInc", address="www", home="rock", work="worldsaver", mobile="123",
                                     fax="123", email="one@home.com", email2="two@home.com", email3="three@home.com",
                                     homepage="ewew", address2="leningrad", phone2="456", notes="what?", aday="5",
                                     amonth="March", ayear="1987", bday="5", bmonth="March", byear="1987",))
-    app.logout()
+    app.session.logout()
