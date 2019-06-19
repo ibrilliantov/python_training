de8994bbd438ae005327abd503cfca9bfbedde64
DenisMac <lassa1@ya.ru>
2019-06-19 12:09:42 +0300

    common fixture in conftest.py

---
+++ fixture/group.py
@@ -0,0 +1,73 @@
+
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_groups_page()
+
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def open_group_home_page(self):
+        wd = self.app.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit edit button
+        wd.find_element_by_name("edit").click()
+        #edit group
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        #submit group update
+        wd.find_element_by_name("update").click()
+        self.return_groups_page()
+
+
+
+
