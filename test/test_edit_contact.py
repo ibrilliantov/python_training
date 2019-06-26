b03baf450eecb28e67eddd109a663718eebd3b9d
DenisMac <lassa1@ya.ru>
2019-06-27 00:11:39 +0300

    return fixture...

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,21 +3,15 @@ from model.contact import Contact
 
 #
 # def test_edit_first_contact_middlename(app):
-#     app.session.login(username="admin", password="secret")
 #     app.contact.edit_first_contact_page(Contact(middlename="EditIrommanEditIrommanEditIrommanEditIromman"))
-#     app.session.logout()
 
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact_page(Contact(middlename="EditIromman", firstname="EditTony", lastname="EditStark", nickname="Editttt", title="EditWOW",
                                company="EditStarkInc", address="www", home="rock", work="worldsaver", mobile="123",
                                fax="Edit123", email="Editone@home.com", email2="Edittwo@home.com", email3="Editthree@home.com",
                                homepage="Editewew", address2="Editleningrad", phone2="Edit456", notes="Editwhat?", aday="6",
                                amonth="June", ayear="1988", bday="6", bmonth="June", byear="1988", ))
-    app.session.logout()
 
 # def test_edit_first_amonth(app):
-#     app.session.login(username="admin", password="secret")
 #     app.contact.edit_first_contact_page(Contact(amonth="May"))
-#     app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,11 +3,7 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="New Group"))
-    app.session.logout()
 
-# def test_edit_first_group_header(app):
-#     app.session.login(username="admin", password="secret")
-#     app.group.edit_first_group(Group(header="New Header"))
-#     app.session.logout()
+def test_edit_first_group_header(app):
+    app.group.edit_first_group(Group(header="New Header"))
