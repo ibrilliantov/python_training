0ec5ab4d3d1863275680cbecdee33239e2d3c11f
DenisMac <lassa1@ya.ru>
2019-07-01 23:35:35 +0300

    redirect optimization

--- fixture/contact.py
+++ fixture/contact.py
@@ -98,6 +98,8 @@ class ContactHelper:
     def open_contact_home_page(self):
         wd = self.app.wd
         wd.get("http://localhost/addressbook/index.php")
+        # if not (wd.current_url.endswith("/index.php") and len(wd.find_element_by_name("searchform")) > 0):
+        #     wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="gdfgsdgdfg", header="gsdsgfsgf", footer="footer"))
 
-def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+# def test_add_empty_group(app):
+#     app.group.create(Group(name="", header="", footer=""))
 
