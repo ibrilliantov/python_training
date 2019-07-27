ff459f0b5396217630693e41de2111d84d5d7594
DenisMac <lassa1@ya.ru>
2019-07-27 12:40:31 +0300

    generator for contacts

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -5,28 +5,28 @@ import random
 import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_number(prefix, maxlen):
-    symbols = string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(middlename=random_string("middlename", 10), firstname=random_string("firstname", 20), lastname=random_string("lastname", 20),
-            nickname=random_string("nickname", 20), title=random_string ("WOW",10), company=random_string ("StarkInc", 3),
-            address=random_string("www", 30), homephone=random_number ("+7", 7), workphone=random_number ("+7", 7), mobilephone=random_number("+", 7),
-                               fax=random_number ("+7", 5), email="one@home.com", email2="two@home.com", email3="three@home.com",
-                               homepage=random_string ("www", 30), address2=random_string ("www", 30), secondaryphone=random_number ("+7", 7),
-                               notes=random_string("www", 30), aday="5", amonth="March", ayear="1987", bday="5", bmonth="March", byear="1987")
-    for i in range(1)
-]
-
-
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+# def random_string(prefix, maxlen):
+#     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#
+# def random_number(prefix, maxlen):
+#     symbols = string.digits
+#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#
+#
+# testdata = [Contact(middlename=random_string("middlename", 10), firstname=random_string("firstname", 20), lastname=random_string("lastname", 20),
+#             nickname=random_string("nickname", 20), title=random_string ("WOW",10), company=random_string ("StarkInc", 3),
+#             address=random_string("www", 30), homephone=random_number ("+7", 7), workphone=random_number ("+7", 7), mobilephone=random_number("+", 7),
+#                                fax=random_number ("+7", 5), email="one@home.com", email2="two@home.com", email3="three@home.com",
+#                                homepage=random_string ("www", 30), address2=random_string ("www", 30), secondaryphone=random_number ("+7", 7),
+#                                notes=random_string("www", 30), aday="5", amonth="March", ayear="1987", bday="5", bmonth="March", byear="1987")
+#     for i in range(1)
+# ]
+#
+#
+#
+#
+# @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
