0a5f2216b1e6264ae7817653292b5bc1b9cd9d7d
DenisMac <lassa1@ya.ru>
2019-08-06 14:48:48 +0300

    Задание №22: Реализовать тесты для добавления контакта в группу и удаления контакта из группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,7 +7,7 @@ def test_add_group(app, db, check_ui, json_groups):
         old_groups = db.get_group_list()
         app.group.create(group)
         new_groups = db.get_group_list()
-        old_groups.append(group)
+        old_groups.append(new_groups)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
         if check_ui:
                 assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
